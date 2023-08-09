9dbad5bfd16b66ff44d527762eea7ea3e412f65e
stslex <ilya977.077@gmail.com>
2023-08-09 22:38:18 +0300

    init webCompose

--- site/src/jsMain/kotlin/com/stslex/WebStSlex/components/widgets/GoHomeLink.kt
+++
@@ -1,7 +0,0 @@
-package com.stslex.WebStSlex.components.widgets
-
-import androidx.compose.runtime.Composable
-import com.varabyte.kobweb.silk.components.navigation.Link
-
-@Composable
-fun GoHomeLink() = Link("/", "Go Home")
